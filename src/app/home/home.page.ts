2710e9ce1eb876d10c1ec21eb8e9b2178533bcef
Tsunokaway <tsunokaway@gmail.com>
2025-05-13 13:06:05 -0300

    anotacoes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,16 +11,36 @@ import { RangeCustomEvent } from '@ionic/angular';
 export class HomePage {
 
   constructor(public router: Router) { }
+  //definindo as variáveis
   temperatura = 0;
   lista_recomendacao_culturas = "";
+  //valor do ion-range:
   temperatura_range(event: RangeCustomEvent) {
     this.temperatura = parseInt(event.detail.value.toString())
   }
 
+  /*
+    *Condições das culturas:*
+    Morangos - 15°C e 25°C
+    Uvas - 20°C 30°C
+    Laranjas - 15°C e 30°C
+    Mirtilos - 0°C e 10°C
+    Maçãs - 15°C e 25°C
+    Ameixas - 15°C e 25°C
+    Acerola - 20°C e 35°C
+    Figos - 20°C e 30°C
+    Romã - 25°C e 35°C
+    Peras - 15°C e 25°C
+   */
+
+
+  //função do botão que encaminha para outra tela
   confirmar() {
-    if (this.temperatura)
-      this.router.navigateByUrl
-        (`/tela-recomendacao-cultura/${this.temperatura}`);
+    if (this.temperatura) {
+
+    } else if (this.temperatura) { }
+    this.router.navigateByUrl
+      (`/tela-recomendacao-cultura/${this.temperatura}`);
   }
 
 
